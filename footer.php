4af0525e1ede65e25158eac7052284b4cd7a5ae7
Sohel <srperves30@gmail.com>
2024-06-07 00:20:13 +0600

    commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><div style="text-align: center;">All Rights Reserves By<br>
+    Mohammad Sohel Parves<br>
+    Copyright © 2023
+  </div>
+  </div>
+
+
+</body>
+
+</html>
---
+++ head.php
@@ -0,0 +1,81 @@
+<html>
+
+<head>
+
+
+<style>
+
+.header {
+  overflow:hidden;
+  background-color: #333;
+  top: 0;
+  width:100%;
+    padding: 3px 5px;
+  color:#FF0404  ;
+}
+
+/* Style the header links */
+.header a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+  font-weight:bold;
+}
+
+
+
+/* Float the link section to the right */
+.header-right {
+  float: right;
+}
+
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .header-right {
+    float: none;
+  }
+}
+/* Style the active/current link*/
+a.act{
+background: linear-gradient(to right, #3b3b3b 0%, #717171 100%);
+color: white;
+border-radius:12px;
+}
+a.logo2{
+  background-color: #333;
+}
+
+img{
+  margin: 0px;
+  padding: 0 0 0; 
+  height: 40px;
+  width: 70px;
+  position: absolute
+}
+</style>
+</head>
+
+<body>
+  <div class="header">
+    <a href="home.php" class="logo"<?php if($active=='home') echo "class='logo2'"; ?>><img src="image/mainlogo.png"></a>
+    <div class="header-right">
+    <a href="about_us.php"  <?php if($active=='about') echo "class='act'"; ?> >About Us</a>
+    <a href="why_donate_blood.php"  <?php if($active=='why') echo "class='act'"; ?>>Why Donate Blood</a>
+      <a href="donate_blood.php"  <?php if($active=='donate') echo "class='act'"; ?>>Become A Donor</a>
+      <a href="need_blood.php" <?php if($active=='need') echo "class='act'"; ?>>Need Blood</a>
+      <a href="contact_us.php" <?php if($active=='contact') echo "class='act'"; ?>>Contact Us</a>
+    </div>
+  </div>
+
+</body>
+</html>
